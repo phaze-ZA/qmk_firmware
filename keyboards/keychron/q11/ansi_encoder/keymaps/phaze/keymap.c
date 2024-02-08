9cef6cb4286021b3d1e7ec0e4f0cb25097d48a10
Luke Swan <swan.luke9@gmail.com>
2024-02-09 01:17:13 +0200

    Moving mouse keys to fn layer and making fn layer Tap-Toggle

--- keyboards/keychron/q11/ansi_encoder/keymaps/phaze/keymap.c
+++ keyboards/keychron/q11/ansi_encoder/keymaps/phaze/keymap.c
@@ -17,8 +17,7 @@
 
 enum layers{
     BASE,
-    FN,
-    MOUSE
+    FN
 };
 
 #define KC_TASK LGUI(KC_TAB)
@@ -27,39 +26,30 @@ enum layers{
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_91_ansi(
         KC_MUTE,  KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_INS,   KC_DEL,   KC_MUTE,
-        TG(MOUSE),  KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
+        _______,  KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
         _______,  KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
         _______,  KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
         _______,  KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
-        _______,  KC_LCTL,  KC_LWIN,  KC_LALT,  MO(FN),         KC_LSFT,                        KC_SPC,             KC_RALT,  MO(FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+        _______,  KC_LCTL,  KC_LGUI,  KC_LALT,  TT(FN),         LSFT_T(KC_SPC),                        RSFT_T(KC_SPC),             KC_RALT,  TT(FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [FN] = LAYOUT_91_ansi(
         RGB_TOG,  _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,      RGB_VAI,     KC_MPRV,     KC_MPLY,      KC_MNXT,      KC_MUTE,  KC_VOLD,    KC_VOLU,  _______,  _______,  RGB_TOG,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,      _______,     _______,     _______,      _______,      _______,  _______,    _______,  _______,            _______,
         _______,  RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,      _______,     _______,     _______,      _______,      _______,  _______,    _______,  _______,            _______,
-        _______,  _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,      _______,  _______,  _______,     _______,  _______,  _______,              _______,            _______,
-        _______,  _______,            _______,  _______,  _______,  _______,      _______,     NK_TOGG,     _______,      _______,      _______,  _______,              _______,  _______,
-        _______,  _______,  _______,  _______,  _______,            _______,                             _______,                 _______,  _______,    _______,  _______,  _______,  _______),
-
-    [MOUSE] = LAYOUT_91_ansi(
-        _______,  _______,  _______,  _______,  KC_TASK,  KC_FLXP,  _______,      _______,     KC_MPRV,     KC_MPLY,      KC_MNXT,      KC_MUTE,  KC_VOLD,    KC_VOLU,  _______,  _______,  _______,
-        _______,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,      KC_NO,     KC_NO,     KC_NO,      KC_NO,      KC_NO,  KC_NO,    KC_NO,  KC_NO,            KC_NO,
-        _______,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,      KC_NO,     KC_NO,     KC_NO,      KC_NO,      KC_NO,  KC_NO,    KC_NO,  KC_NO,            KC_NO,
-        _______,  KC_NO,  KC_NO, KC_WH_L,  KC_WH_U,  KC_WH_D,  KC_WH_R,      KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_UP,      KC_MS_RIGHT,      KC_NO,  KC_NO,              KC_NO,            KC_NO,
-        _______,  _______,            KC_NO,  KC_NO,  KC_NO,  KC_NO,      KC_NO,     KC_NO,     KC_NO,      KC_NO,      KC_NO,  KC_NO,              KC_NO,  KC_NO,
-        _______,  _______,  _______,  _______,  KC_NO,            KC_MS_BTN2,                             KC_MS_BTN1,                 _______,  KC_NO,    _______,  KC_NO,  KC_NO,  KC_NO),
+        _______,  _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,      KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_UP,      KC_MS_RIGHT,  _______,  _______,              _______,            _______,
+        _______,  _______,            _______,  _______,  _______,  _______,      _______,     NK_TOGG,     _______,      _______,      _______,  _______,              _______,  KC_WH_D,
+        _______,  _______,  _______,  _______,  _______,            KC_MS_BTN2,                             KC_MS_BTN1,                 _______,  _______,    _______,  KC_WH_R ,  KC_WH_U,  KC_WH_L),
 };
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
     [FN]   = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI), ENCODER_CCW_CW(RGB_VAD, RGB_VAI) },
-    [MOUSE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) }
 };
 #endif // ENCODER_MAP_ENABLE
 
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-    if (get_highest_layer(layer_state) == 2) {
+    if (get_highest_layer(layer_state) == 1) {
         uint8_t layer = get_highest_layer(layer_state);
         HSV hsv = rgb_matrix_get_hsv();
         RGB color = hsv_to_rgb(hsv);
