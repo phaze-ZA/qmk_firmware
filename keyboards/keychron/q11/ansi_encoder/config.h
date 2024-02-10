d6cd767ee2357355f8fc9cf1b9b41f929d4ab2d7
Luke Swan <swan.luke9@gmail.com>
2024-02-11 00:19:24 +0200

    Adding a gaming layer

--- keyboards/keychron/q11/ansi_encoder/config.h
+++ keyboards/keychron/q11/ansi_encoder/config.h
@@ -26,7 +26,7 @@
 #define TAPPING_TOGGLE 2
 
 // Configure the global tapping term (default: 200ms)
-#define TAPPING_TERM 150
+#define TAPPING_TERM 200
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define QUICK_TAP_TERM 0
